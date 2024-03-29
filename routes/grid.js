eb77816a8fdb20dfb7f95349b79cc7fb18e0e40f
TBoomz <111537214+TBoomz@users.noreply.github.com>
2024-03-28 20:42:32 -0500

    Update grid.js

--- routes/grid.js
+++ routes/grid.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('grid', { title: 'Grid Display' });
+  res.render('grid', { title: 'Grid Display', query: query });
   let query = req.query
     console.log(`rows ${query.rows}`)
     console.log(`cols ${query.cols}`)
