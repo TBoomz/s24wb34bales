3df984c32acead1f8043e31e003df38f8482d435
TBoomz <111537214+TBoomz@users.noreply.github.com>
2024-03-28 20:45:00 -0500

    Update grid.js

--- routes/grid.js
+++ routes/grid.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET grid page. */
 router.get('/', function(req, res, next) {
+  // Retrieve the query parameters from the request object
+  let query = req.query;
+
+  // Log the query parameters for debugging
+  console.log(`rows ${query.rows}`);
+  console.log(`cols ${query.cols}`);
+
+  // Render the grid.pug template and pass the title and query object to it
   res.render('grid', { title: 'Grid Display', query: query });
-  let query = req.query
-    console.log(`rows ${query.rows}`)
-    console.log(`cols ${query.cols}`)
 });
 
 module.exports = router;
